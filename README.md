f2e3eeb14568a628c3bf9a6991968aa5f9d2ec05
Ben Miller <bvmiller@mail.sfsu.edu>
2020-03-09 11:08:42 -0700

    init

---
+++ README.md
@@ -0,0 +1,22 @@
+# DigitalOcean Dynamic DNS
+
+A command-line tool to automatically update your DigitalOcean DNS records with your current IP address. Only `A` records for a single root domain (e.g., `example.com`) are supported at this time.
+
+## Requirements
+
+- [PHP](https://www.php.net/downloads.php)
+- [Composer](https://getcomposer.org)
+- A DigitalOcean [API token](https://cloud.digitalocean.com/account/api/tokens)
+- A task scheduler (such as *cron* or *Windows Task Scheduler*)
+
+## Setup
+
+The following assumes a Linux host, you will need to modify as needed if you are running Windows or macOS; this script should work on any platform.
+
+**Note:** The record(s) you want updated must already exist on DigitalOcean.
+
+- Clone or download this repository to a new directory of your choice
+- Install required dependencies: `composer install`
+- Setup the environment: `cp .env.example .env && nano .env`
+- Test run: `php update.php`, if you see any errors your `.env` file is probably incorrectly configured
+- Create a scheduled task. I use this in my `crontab`: `0 * * * * php /home/benjivm/digitalocean-ddns/update.php`
---
+++ update.php
@@ -0,0 +1,101 @@
+<?php
+
+require __DIR__ . '/vendor/autoload.php';
+
+use Dotenv\Dotenv;
+use Symfony\Component\HttpClient\HttpClient;
+
+/*
+* $dotenv
+*
+* Load our .env file.
+*/
+
+$dotenv = Dotenv::createImmutable(__DIR__);
+$dotenv->load();
+
+/*
+* $httpClient
+*
+* This is our HTTP Client.
+*/
+$httpClient = HttpClient::create();
+
+/*
+* $getIp()
+*
+* This function fetches our IP from
+* the endpoint defined in .env.
+*/
+$getIp = function () use ($httpClient) {
+    $ipRequest = $httpClient->request('GET', getenv('CHECKIP_URL'));
+
+    return trim($ipRequest->getContent());
+};
+
+/*
+* $getHostnames()
+*
+* This function parses the HOSTNAMES
+* variable defined in .env, it
+* returns an array.
+*/
+$getHostnames = function () {
+    $hostnames = getenv('HOSTNAMES');
+
+    if (strpos($hostnames, ',') !== false) {
+        return explode(',', $hostnames);
+    }
+
+    return [$hostnames];
+};
+
+/*
+* $getDomainRecords()
+*
+* This function fetches our domain's
+* DNS records from DigitalOcean.
+*/
+$getDomainRecords = function () use ($httpClient) {
+    $dnsRecordsRequest = $httpClient->request(
+        'GET',
+        'https://api.digitalocean.com/v2/domains/' . getenv('DOMAIN') . '/records',
+        ['auth_bearer' => getenv('DO_API_TOKEN')]
+    );
+
+    return $dnsRecordsRequest->toArray()['domain_records'];
+};
+
+/*
+* $updateDnsRecord($record, $data)
+*
+* This function updates a DNS record's
+* value with the given data.
+*/
+$updateDnsRecord = function ($record, $data) use ($httpClient) {
+    $endpoint = sprintf(
+        'https://api.digitalocean.com/v2/domains/%s/records/%s',
+        getenv('DOMAIN'),
+        $record['id']
+    );
+
+    $httpClient->request('PUT', $endpoint, [
+        'auth_bearer' => getenv('DO_API_TOKEN'),
+        'json'        => ['data' => $data, 'ttl' => 30],
+    ]);
+};
+
+/*
+* Execute the update.
+*/
+$records = $getDomainRecords();
+$hostnames = $getHostnames();
+$ip = $getIp();
+
+foreach ($hostnames as $host) {
+    $record = current(array_filter($records, function ($value) use ($host) {
+        return $value['name'] === $host;
+    }));
+
+    $updateDnsRecord($record, $ip);
+}
